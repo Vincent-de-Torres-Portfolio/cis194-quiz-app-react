ffdad108270580cf051f2126277c7e96d4560ba6
Vincent de Torres <vdetorres@go.pasadena.edu>
2023-09-09 09:18:55 -0700

    added inline comments & updated `READme.md`

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-Starter code for quiz-app beginner react project
 
-- clone repo
-- npm install
-- npm start
+# CIS194_Homework-Assignment-1_React-Quiz-App
+
+---
+
+```
+Vincent de Torres
+devinci-it
+CIS 194 (Fall 2023)
+Homework Assignment 1
+09/09/2023
+```
+
+# Description
+The React Quiz App is a hands-on project created as a course requirement, following the tutorial from [FreeCodeCamp](https://www.freecodecamp.org/news/how-to-build-a-quiz-app-using-react/). This project is part of a collection of hands-on demo projects aimed for practical demonstration of React concepts learned throughout the course.
+
+## About This Project
+
+This project is not designed for production use but rather as a learning exercise to:
+
+- Apply React.js concepts and syntax learned in the course.
+- Gain practical experience in building React applications.
+---
+
+The app will have these basic features:
+- When the user clicks a button, the next question should show
+- If the user gets the question correct, it should increment their score
+- When the user gets to the end of the quiz, their total score should be shown
+
+## Live Demo
+
+I have deployed the React Quiz App using GitHub Pages, and you can access the live demo by clicking the link below:
+
+[**Live Demo**](https://vincent-de-torres-portfolio.github.io/cis194-quiz-app-react/)
--- src/App.js
+++ src/App.js
@@ -1,89 +1,92 @@
 import React, { useState } from 'react';
 
 export default function App() {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
+  // Define an array of questions and their answer options
+  const questions = [
+    {
+      questionText: 'What is the capital of France?',
+      answerOptions: [
+        { answerText: 'New York', isCorrect: false },
+        { answerText: 'London', isCorrect: false },
+        { answerText: 'Paris', isCorrect: true },
+        { answerText: 'Dublin', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who is CEO of Tesla?',
+      answerOptions: [
+        { answerText: 'Jeff Bezos', isCorrect: false },
+        { answerText: 'Elon Musk', isCorrect: true },
+        { answerText: 'Bill Gates', isCorrect: false },
+        { answerText: 'Tony Stark', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'The iPhone was created by which company?',
+      answerOptions: [
+        { answerText: 'Apple', isCorrect: true },
+        { answerText: 'Intel', isCorrect: false },
+        { answerText: 'Amazon', isCorrect: false },
+        { answerText: 'Microsoft', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'How many Harry Potter books are there?',
+      answerOptions: [
+        { answerText: '1', isCorrect: false },
+        { answerText: '4', isCorrect: false },
+        { answerText: '6', isCorrect: false },
+        { answerText: '7', isCorrect: true },
+      ],
+    },
+  ];
 
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
-	const handleAnswerButtonClick = (isCorrect) => {
-		const nextQuestion = currentQuestion + 1;
-		if (isCorrect) {
-		  setScore(score + 1);
-		}
-	  
-		if (nextQuestion < questions.length) {
-		  setCurrentQuestion(nextQuestion);
-		} else {
-		  setShowScore(true);
-		}
-	  };
-	  
-	
-	return (
-		<div className='app'>
-			{/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
-			{showScore ? (
-				<div className='score-section'>You scored {score} out of {questions.length}</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1 }</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
+  // Function to handle the click of an answer button
+  const handleAnswerButtonClick = (isCorrect) => {
+    const nextQuestion = currentQuestion + 1;
+    if (isCorrect) {
+      // If the answer is correct, increment the score
+      setScore(score + 1);
+    }
 
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption, index) => (
-					<button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-					))}
-					</div>
+    if (nextQuestion < questions.length) {
+      // If there are more questions, move to the next question
+      setCurrentQuestion(nextQuestion);
+    } else {
+      // If there are no more questions, show the final score
+      setShowScore(true);
+    }
+  };
 
-				</>
-				
-			)}
-		</div>
-	);
-	
+  return (
+    <div className='app'>
+      {showScore ? (
+        // Display the score when all questions have been answered
+        <div className='score-section'>You scored {score} out of {questions.length}</div>
+      ) : (
+        <>
+          <div className='question-section'>
+            <div className='question-count'>
+              {/* Display the current question number */}
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text'>{questions[currentQuestion].questionText}</div>
+          </div>
+
+          <div className='answer-section'>
+            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+              // Map over answer options and create buttons for each option
+              <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)} key={index}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
-
