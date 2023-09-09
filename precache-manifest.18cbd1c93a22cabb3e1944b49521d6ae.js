171f6d70980f312c2d3c7f26143a10eea3d0e3f5
Vincent de Torres <vdetorres@go.pasadena.edu>
2023-09-09 08:25:14 -0700

    Updates

---
+++ precache-manifest.18cbd1c93a22cabb3e1944b49521d6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5f9c17d6ef118b7988d939e6be5e44",
+    "url": "/cis194-quiz-app-react/index.html"
+  },
+  {
+    "revision": "b4e37a9c90482423e618",
+    "url": "/cis194-quiz-app-react/static/css/main.5a032b4e.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/cis194-quiz-app-react/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cis194-quiz-app-react/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e37a9c90482423e618",
+    "url": "/cis194-quiz-app-react/static/js/main.f3eee7a2.chunk.js"
+  },
+  {
+    "revision": "e5e23e16e60ed9de2746",
+    "url": "/cis194-quiz-app-react/static/js/runtime-main.02e3ff32.js"
+  }
+]);
